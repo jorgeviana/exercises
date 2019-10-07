6d746f9b0c42b7358f2a569f33fb45e74f57bfd9
Jorge Viana <secoviana@gmail.com>
2019-10-07 22:10:59 +0200

    refactoring tests

--- src/test/java/com/example/tellyo/sets/SocialMediaNetworksTest.java
+++ src/test/java/com/example/tellyo/sets/SocialMediaNetworksTest.java
@@ -1,27 +1,32 @@
 package com.example.tellyo.sets;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SocialMediaNetworksTest {
 
+    private static final String NETWORKS_ONE = "twitter,facebook";
+    private static final String NETWORKS_TWO = "facebook,youtube,blablabla";
+
+    private SocialMediaNetworks socialMediaNetworks;
+
+    @Before
+    public void setUp() {
+        socialMediaNetworks = new SocialMediaNetworks();
+    }
+
     @Test
     public void and() {
-        String networksOne = "twitter,facebook";
-        String networksTwo = "facebook,youtube,blablabla";
-
-        String andResult = new SocialMediaNetworks().and(networksOne, networksTwo);
+        String andResult = socialMediaNetworks.and(NETWORKS_ONE, NETWORKS_TWO);
 
         assertThat(andResult.split(",")).containsExactlyInAnyOrder("facebook");
     }
 
     @Test
     public void or() {
-        String networksOne = "twitter,facebook";
-        String networksTwo = "facebook,youtube,blablabla";
-
-        String orResult = new SocialMediaNetworks().or(networksOne, networksTwo);
+        String orResult = socialMediaNetworks.or(NETWORKS_ONE, NETWORKS_TWO);
 
         assertThat(orResult.split(",")).containsExactlyInAnyOrder("twitter", "facebook", "youtube");
     }
