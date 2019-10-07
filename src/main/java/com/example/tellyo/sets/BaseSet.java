9267ecf74397ad20bc4de92b7d0404f57bb1bf45
Jorge Viana <secoviana@gmail.com>
2019-10-07 22:27:42 +0200

    refactoring base class

--- src/main/java/com/example/tellyo/sets/BaseSet.java
+++ src/main/java/com/example/tellyo/sets/BaseSet.java
@@ -10,34 +10,40 @@ public abstract class BaseSet {
 
     abstract Set<String> getValidTokens();
 
-    public String and(String set1, String set2) {
-        Set<String> s1 = split(set1);
-        Set<String> s2 = split(set2);
+    public String and(String string1, String sestring2) {
+        Set<String> s1 = removeInvalidTokens(split(string1));
+        Set<String> s2 = removeInvalidTokens(split(sestring2));
 
-        s1.retainAll(getValidTokens());
-        s2.retainAll(getValidTokens());
-
-        s1.retainAll(s2);
-
-        return format(s1);
+        return format(and(s1, s2));
     }
 
-    public String or(String set1, String set2) {
-        Set<String> s1 = split(set1);
-        Set<String> s2 = split(set2);
+    public String or(String string1, String string2) {
+        Set<String> s1 = removeInvalidTokens(split(string1));
+        Set<String> s2 = removeInvalidTokens(split(string2));
 
-        s1.retainAll(getValidTokens());
-        s2.retainAll(getValidTokens());
-
-        Set<String> result = new HashSet<>();
-        result.addAll(s1);
-        result.addAll(s2);
-
-        return format(result);
+        return format(or(s1, s2));
     }
 
     private HashSet<String> split(String networksOne) {
         return new HashSet<>(asList(networksOne.split(",")));
+    }
+
+    private Set<String> removeInvalidTokens(Set<String> s) {
+        Set<String> result = new HashSet<>(s);
+        result.retainAll(getValidTokens());
+        return result;
+    }
+
+    private Set<String> and(Set<String> s1, Set<String> s2) {
+        Set<String> result = new HashSet<>(s1);
+        result.retainAll(s2);
+        return result;
+    }
+
+    private Set<String> or(Set<String> s1, Set<String> s2) {
+        Set<String> result = new HashSet<>(s1);
+        result.addAll(s2);
+        return result;
     }
 
     private String format(Set<String> result) {
