4ac7ea0bc03c1790dce34f1d3d0cef6a6e896b60
Jorge Viana <secoviana@gmail.com>
2019-10-07 21:49:42 +0200

    refactoring - removing duplication

--- src/main/java/com/example/tellyo/sets/AllowedActions.java
+++ src/main/java/com/example/tellyo/sets/AllowedActions.java
@@ -1,53 +1,12 @@
 package com.example.tellyo.sets;
 
 import java.util.HashSet;
-import java.util.Set;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.joining;
 
 public class AllowedActions extends BaseSet {
 
     public AllowedActions() {
         tokens = new HashSet<>(asList("add", "delete", "modify", "clear", "hide"));
-    }
-
-    public String and(String set1, String set2) {
-        Set<String> n1 = split(set1);
-        Set<String> n2 = split(set2);
-
-        n1.retainAll(getValidTokens());
-        n2.retainAll(getValidTokens());
-
-        n1.retainAll(n2);
-
-        return format(n1);
-    }
-
-    public String or(String set1, String set2) {
-        Set<String> s1 = split(set1);
-        Set<String> s2 = split(set2);
-
-        s1.retainAll(getValidTokens());
-        s2.retainAll(getValidTokens());
-
-        Set<String> result = new HashSet<>();
-        result.addAll(s1);
-        result.addAll(s2);
-
-        return format(result);
-    }
-
-    private Set<String> getValidTokens() {
-        return tokens;
-    }
-
-    private HashSet<String> split(String networksOne) {
-        return new HashSet<>(asList(networksOne.split(",")));
-    }
-
-    private String format(Set<String> result) {
-        return result.stream()
-                .collect(joining(","));
     }
 }
